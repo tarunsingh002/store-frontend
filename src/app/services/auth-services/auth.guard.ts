d589b4b60234326a08eecc64b343498d546a5614
Tarun Singh <tarun.singh1120@gmail.com>
2024-07-16 07:36:33 +0530

    auth guards bug

--- src/app/services/auth-services/auth.guard.ts
+++ src/app/services/auth-services/auth.guard.ts
@@ -1,27 +1,23 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
-import { AuthService } from "./auth.service";
-import { map, take } from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+import {map, take} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
-export class AuthGuard  {
+export class AuthGuard {
   constructor(private authS: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authS.User.pipe(
       take(1),
       map((user) => {
         if (!!user) return !!user;
-        else return this.router.createUrlTree(["/auth"]);
+        else return this.router.createUrlTree(['/auth', 'signin']);
       })
     );
   }
--- src/app/services/auth-services/is-webmaster.guard.ts
+++ src/app/services/auth-services/is-webmaster.guard.ts
@@ -1,30 +1,26 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
-import { AuthService } from "./auth.service";
-import { map, take } from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+import {map, take} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
-export class IsWebmasterGuard  {
+export class IsWebmasterGuard {
   constructor(private authS: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authS.User.pipe(
       take(1),
       map((user) => {
         if (!!user && user.webmaster) return true;
         else if (!!user && !user.webmaster) {
           this.authS.logout();
-          return this.router.createUrlTree(["/auth"]);
-        } else if (!user) return this.router.createUrlTree(["/auth"]);
+          return this.router.createUrlTree(['/auth', 'signin']);
+        } else if (!user) return this.router.createUrlTree(['/auth', 'signin']);
       })
     );
   }
