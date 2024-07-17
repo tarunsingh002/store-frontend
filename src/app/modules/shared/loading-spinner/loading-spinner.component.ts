3a56cd8452c77095484ba1113c23179a1b45539c
Tarun Singh <tarun.singh1120@gmail.com>
2024-07-17 11:57:13 +0530

    removed ngif from router outlet

--- src/app/modules/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/modules/shared/loading-spinner/loading-spinner.component.ts
@@ -4,6 +4,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.css'],
+  host: {class: 'd-flex justify-content-center align-items-center w-100'},
 })
 export class LoadingSpinnerComponent implements OnInit, OnDestroy {
   wakingUpTheServerSideCode = false;
